db42190df202d8d2ae5fca57f253ef0ad33eceb8
Ratna Sambhav <72562461+Ratna-Sambhav@users.noreply.github.com>
2024-03-31 14:10:57 +0530

    Create cuda_driver_install.sh

---
+++ backend/cuda_driver_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get install -y gcc
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda-repo-ubuntu2204-12-4-local_12.4.0-550.54.14-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-4-local_12.4.0-550.54.14-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-4-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-4
+
+sudo apt-get install -y cuda-drivers
+sudo apt-get install -y nvidia-driver-550-open
+sudo apt-get install -y cuda-drivers-550
